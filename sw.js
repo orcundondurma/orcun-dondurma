0306ce45f02cd39301ed7ae432f535c463bad3c3
orcundondurma <kanalcemre@gmail.com>
2026-05-17 04:44:11 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// Orçun Dondurma — Service Worker v0.7.4
-const VERSION = "v0.7.4";
+// Orçun Dondurma — Service Worker v0.7.5
+const VERSION = "v0.7.5";
 const CACHE = "orcun-" + VERSION;
 
 const ASSETS = [
